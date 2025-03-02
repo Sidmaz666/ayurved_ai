3d6ba34523b05a5c74aec95be4191e96a635df0c
sidmaz666 <sidmazumder8@gmail.com>
2025-03-02 10:10:21 +0530

    fix(index): proper path with __dirname

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join("public")));
+app.use(express.static(path.join(__dirname,"public")));
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "localhost";
